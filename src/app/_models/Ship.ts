db5b742d5e3cbce7c4ca313b15a49d37d9b9204a
Niksior <heroes.vs.misio@gmail.com>
2019-12-15 15:49:57 +0100

    o kurwa

--- src/app/_models/Ship.ts
+++ src/app/_models/Ship.ts
@@ -1,16 +1,37 @@
 export class Ship {
+
   constructor(type: string) {
+
     this._type = type;
+
+    // switch (type) {
+    //   case '4mast':
+    //     this._lifeNum = 4;
+    //     break;
+    //   case '3mast':
+    //     this._lifeNum = 3;
+    //     break;
+    //   case '2mast':
+    //     this._lifeNum = 2;
+    //     break;
+    //   case '1mast':
+    //     this._lifeNum = 1;
+    //     break;
+    //   default:
+    //     this._lifeNum = 1;
+    //     break;
+    // }
+    // ziomek z wykladu powiedzial, ze polimorfizm a nie ify jebane
     if (type === '4mast') {
       this._lifeNum = 4;
     } else if (type === '3mast') {
       this._lifeNum = 3;
     } else if (type === '2mast') {
       this._lifeNum = 2;
-
     } else {
       this._lifeNum = 1;
     }
+
     this._id = Math.random();
     this._shoots = 0;
   }
