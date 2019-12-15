0fd4db4066c45efff9946cd30a781ecf6ccfd5a6
aredlinski <aredlinski@sii.pl>
2019-12-15 09:20:56 +0100

    refactor

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -10,56 +10,52 @@ import {Mapa} from "../_models/Mapa";
 export class BoardComponent {
 
   ships: Ship[];
-  mapSize : number;
-  map : Mapa | null;
+  mapSize: number;
+  map: Mapa | null;
 
   constructor() {
     let settingData = localStorage.getItem('settingsData');
-    let data;
     this.ships = [];
     this.mapSize = 10;
 
-    if (settingData != null) {
-      data = JSON.parse(settingData);
-      console.log('data: ' + settingData);
-      this.map = new Mapa(data);
-
-      let oneMast = data.oneMastShips;
-      let twoMast = data.twoMastShips;
-      let threeMast = data.threeMastShips;
-      let fourMast = data.fourMastShips;
-      this.mapSize = data.height;
-
-      for (let i = 0; i < oneMast; i++) {
-        let ship = new Ship('1mast');
-        this.ships.push(ship);
-      }
-      for (let i = 0; i < twoMast; i++) {
-        let ship = new Ship('2mast');
-        this.ships.push(ship);
-      }
-      for (let i = 0; i < threeMast; i++) {
-        let ship = new Ship('3mast');
-        this.ships.push(ship);
-      }
-      for (let i = 0; i < fourMast; i++) {
-        let ship = new Ship('4mast');
-        this.ships.push(ship);
-      }
-    }
-    else
-    {
+    if (settingData == null) {
       this.map = null;
+      return;
+    }
+
+    let data = JSON.parse(settingData);
+    console.log('data: ' + settingData);
+    this.map = new Mapa(data);
+
+    let oneMast = data.oneMastShips;
+    let twoMast = data.twoMastShips;
+    let threeMast = data.threeMastShips;
+    let fourMast = data.fourMastShips;
+    this.mapSize = data.size;
+
+    for (let i = 0; i < oneMast; i++) {
+      let ship = new Ship('1mast');
+      this.ships.push(ship);
+    }
+    for (let i = 0; i < twoMast; i++) {
+      let ship = new Ship('2mast');
+      this.ships.push(ship);
+    }
+    for (let i = 0; i < threeMast; i++) {
+      let ship = new Ship('3mast');
+      this.ships.push(ship);
+    }
+    for (let i = 0; i < fourMast; i++) {
+      let ship = new Ship('4mast');
+      this.ships.push(ship);
     }
   }
 
   shoot(ship: Ship) {
     console.log('on click card' + JSON.stringify(ship));
-    if (this.map != null)
-    {
+    if (this.map != null) {
       this.map.shoots += 1;
-      if (ship == null)
-      {
+      if (ship == null) {
         this.map.missed += 1;
         alert('pudło');
 
