dbc61e8e210ac329dfe7c09be8681a8f5db0f749
Niksior <heroes.VS.misio@gmail.com>
2019-12-31 17:20:35 +0100

    bez smieci

--- src/app/_enums/ship-type.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum ShipType {
-  OneMast = 1,
-  TwoMast,
-  ThreeMast,
-  FourMast
-}
--- src/app/_interfaces/ship.ts
+++
@@ -1,9 +0,0 @@
-import {ShipType} from '../_enums/ship-type.enum';
-
-export interface Ship {
-  shipType: ShipType;
-  id: number;
-  hp: number;
-
-  hit(): void;
-}
